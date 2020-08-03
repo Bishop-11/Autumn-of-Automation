c89e7d526daada96a8b27e7597749d9e89a22446
Bishop-11 <prakashbishop7@gmail.com>
2020-08-03 22:34:27 +0530

    Gazebo Wosrkspace Adde

---
+++ Gazebo/bot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/bishop/bot_ws/src
+# Build directory: /home/bishop/bot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(bot_control)
+subdirs(bot_description)
+subdirs(bot_gazebo)
